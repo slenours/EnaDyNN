bfb6fbc8ec19fe03fe705374c9be8b5fc736a7e8
Yongjie178 <106809201+Yongjie178@users.noreply.github.com>
2022-09-15 15:58:24 +0200

    Update imports.py

--- imports.py
+++ imports.py
@@ -17,16 +17,16 @@ from math import exp
 from tqdm import tqdm
 from skimage.util import crop
 from torchvision import models
-from geomloss import SamplesLoss
+#from geomloss import SamplesLoss
 from psutil import virtual_memory
 from torchvision import transforms
 from collections import OrderedDict
 from torch.utils.data import Dataset
 from tensorboardX import SummaryWriter
-from kornia.enhance import image_histogram2d
-from scipy.stats import binned_statistic, norm
+#from kornia.enhance import image_histogram2d
+#from scipy.stats import binned_statistic, norm
 from torch.utils.data import random_split, DataLoader
 from torch.optim.lr_scheduler import StepLR
-from skimage.transform import resize, warp, AffineTransform
+#from skimage.transform import resize, warp, AffineTransform
 
 _VALID_SCENE_TYPES = ('decalage', 'plan')
